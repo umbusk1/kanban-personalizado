e5afb787cb4de259bbab55934cffe69b9deda356
Umbusk <moises@umbusk.com>
2026-02-19 16:56:48 -0400

    Update AppHeader.tsx

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -16,7 +16,7 @@ export default function AppHeader() {
 
   return (
     <header className="bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-700 sticky top-0 z-40">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-20 flex items-center justify-between">
 
         {/* Logo */}
         <Link href="/dashboard" className="flex items-center group">
