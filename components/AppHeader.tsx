6456e418353a7bedcc199fe8c460403e0660b052
Umbusk <moises@umbusk.com>
2026-03-15 16:02:23 -0400

    Update AppHeader.tsx

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -17,7 +17,7 @@ export default function AppHeader() {
     <header className="bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-700 sticky top-0 z-40">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-20 flex items-center justify-between">
         {/* Logo */}
-        <Link href="/dashboard" className="h-[60px] w-auto">
+        <Link href="/dashboard" className="h-[50px] w-auto">
           <Image
             src="/logo.svg"
             alt="kanbanbonsai"
