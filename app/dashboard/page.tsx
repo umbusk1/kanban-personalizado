db2ed841c745fbe2c501e7f80a46b8fea24d9e7a
Umbusk <moises@umbusk.com>
2026-03-15 18:26:50 -0400

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -199,7 +199,9 @@ export default function DashboardPage() {
 
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 flex flex-col">
-      <AppHeader />
+        <div className="sticky top-0 z-30">
+          <AppHeader />
+        </div>
 
       <main className="flex-1 max-w-[1400px] mx-auto w-full px-4 sm:px-6 lg:px-8 py-8">
 
